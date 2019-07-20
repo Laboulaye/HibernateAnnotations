12ccf911ad9feb2664b0d3440ec94465c2acfcc0
Maksim Demianov <maks.demianov@gmail.com>
2019-07-20 16:21:06 +0300

    Добавлена реализация классов App и Factory

--- src/main/java/com/study/App.java
+++ src/main/java/com/study/App.java
@@ -1,4 +1,47 @@
 package com.study;
 
+import model.Item;
+import model.Provider;
+import model.Warehouse;
+import org.hibernate.Session;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
 public class App {
+    public void printAllWarehouses() {
+        Session session = null;
+        try {
+            Collection<Warehouse> warehouses = Factory.getInstance().getWarehouseDAO().getAllWarehouses();
+            Iterator<Warehouse> warehouseIterator = warehouses.iterator();
+            System.out.println("List of warehouses: ");
+            while (warehouseIterator.hasNext()){
+                Warehouse warehouse = warehouseIterator.next();
+                System.out.println("warehouse number " + warehouse.getId() + ", address is " + warehouse.getAddress());
+                Collection<Item> items = warehouse.getItems();
+                Iterator<Item> itemIterator = items.iterator();
+                while (itemIterator.hasNext()) {
+                    Item item = itemIterator.next();
+                    System.out.println(" has " + item.getName() + ", id of this item is " + item.getId());
+                    Set<Provider> providers = item.getProviders();
+                    Iterator<Provider> providerIterator = providers.iterator();
+                    while (providerIterator.hasNext()){
+                        Provider provider = providerIterator.next();
+                        System.out.println(" which is provided by " + provider.getName() + ", id of this provider is " + provider.getId());
+                    }
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e ){
+            e.printStackTrace();
+        }
+        finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
 }
--- src/main/java/com/study/Factory.java
+++ src/main/java/com/study/Factory.java
@@ -1,4 +1,44 @@
 package com.study;
 
+import dao.ItemDAO;
+import dao.ProviderDAO;
+import dao.WarehouseDAO;
+import daoImpl.ItemDAOImpl;
+import daoImpl.ProviderDAOImpl;
+import daoImpl.WarehouseDAOImpl;
+
 public class Factory {
+
+    private static Factory instance;
+    private static ItemDAO itemDAO;
+    private static ProviderDAO providerDAO;
+    private static WarehouseDAO warehouseDAO;
+
+    public static synchronized Factory getInstance(){
+        if (instance == null) {
+            instance = new Factory();
+        }
+        return instance;
+    }
+
+    public static ItemDAO getItemDAO() {
+        if (itemDAO == null) {
+            itemDAO = new ItemDAOImpl();
+        }
+        return itemDAO;
+    }
+
+    public static ProviderDAO getProviderDAO() {
+        if (providerDAO == null) {
+            providerDAO = new ProviderDAOImpl();
+        }
+        return providerDAO;
+    }
+
+    public static WarehouseDAO getWarehouseDAO() {
+        if (warehouseDAO == null) {
+            warehouseDAO = new WarehouseDAOImpl();
+        }
+        return warehouseDAO;
+    }
 }
